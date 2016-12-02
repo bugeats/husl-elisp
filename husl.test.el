8ac86b3e1257d83bfa22fef34a67702dac01bf83
Chadwick Dahlquist <chadwick@bugeats.net>
2016-12-02 13:18:41 -0800

    intro ert tests

--- husl.test.el
+++ husl.test.el
@@ -1,5 +1,9 @@
 (require 'cl)
 (load-file "./husl.el")
 
-(assert (= (husl/conv-xyz-luv 0.5 0.5 0.5)
-           '(-1763.9307389858443 -291.14032968045854 -123.00736194996507)))
+(ert-deftest addition-test ()
+  (should (= (+ 1 2) 3)))
+
+(ert-deftest test-conv-xyz-luv ()
+  (should (equal (husl/conv-xyz-luv 0.5 0.5 0.5)
+             '(-1763.9307389858443 -291.14032968045854 -123.00736194996507))))
