bc2219655578edf9b02818a4e278134079dc3f94
Chadwick Dahlquist <chadwick@bugeats.net>
2016-12-02 15:30:29 -0800

    dynamically generating ert tests from ref data

--- reference.test.el
+++ reference.test.el
@@ -1,1 +1,18 @@
 (require 'json)
+
+(defun get-string-from-file (filePath)
+  "Return filePath's file content as a string."
+  (with-temp-buffer
+    (insert-file-contents filePath)
+    (buffer-string)))
+
+(setq ref-data
+      (json-read-from-string (get-string-from-file "./snapshot-rev4.json")))
+
+(let (value)
+  (dolist (element ref-data value)
+    (let ((hex (car element)))
+      (eval
+        `(ert-deftest ,hex ()
+            (should (= (+ 2 2) 4)))))))
+
--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-emacs -batch -l ert -l husl.test.el -f ert-run-tests-batch-and-exit
+# emacs -batch -l ert -l husl.test.el -f ert-run-tests-batch-and-exit
+emacs -batch -l ert -l reference.test.el -f ert-run-tests-batch-and-exit
